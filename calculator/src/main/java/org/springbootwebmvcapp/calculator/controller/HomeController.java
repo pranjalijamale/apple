058b6ede892e552d1b8de4dded72d06ff1f9b188
Yogesh_Yeole <Yogesh_Yeole@LAPTOP-UPU3TO81.bbrouter>
2024-12-31 12:21:00 +0530

    dev1

--- calculator/src/main/java/org/springbootwebmvcapp/calculator/controller/HomeController.java
+++ calculator/src/main/java/org/springbootwebmvcapp/calculator/controller/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 	
+	
+	int x;
 	@RequestMapping("/")
 	public String calculat() {
 		return "calculator";
